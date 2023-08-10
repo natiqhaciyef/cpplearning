888fbfe0ff48653b0263f21562c7a7de5a1f5928
natiqhaciyef <natiq00h2272@gmail.com>
2023-08-11 00:45:15 +0400

    Mathematics functions usage added

---
+++ basics/Mathematics.h
@@ -0,0 +1,33 @@
+//
+// Created by Natiq Haciyev on 10.08.23.
+//
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+#ifndef CPPLEARNING_MATHEMATICS_H
+#define CPPLEARNING_MATHEMATICS_H
+
+void mathFunctionsUsage() {
+    // finds square root
+    cout << sqrt(64) << endl;
+
+    // rounds a number
+    cout << round(3.14) << endl;
+
+    // natural logarithm
+    cout << log(2.2) << endl;
+
+    // rounds up to it's the nearest number
+    cout << ceil(34.1) << endl;
+
+    // rounds down to it's the nearest number
+    cout << floor(34.1) << endl;
+
+    // power
+    cout << pow(4, 2) << endl;
+}
+
+
+#endif //CPPLEARNING_MATHEMATICS_H
--- main.cpp
+++ main.cpp
@@ -2,11 +2,12 @@
 #include "basics/Variables.h"
 #include "basics/Operators.h"
 #include "basics/StringFunctions.h"
+#include "basics/Mathematics.h"
 using namespace std;
 
 
 int main() {
-    stringFunctionsUsage();
+    mathFunctionsUsage();
     return 0;
 }
 
